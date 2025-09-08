d8b030055b51251882927db841aee7907878a039
ba32 <167793470+3baa@users.noreply.github.com>
2025-09-08 22:51:58 +0300

    Update and rename C.cpp to multiplication table.cpp

---
+++ Day-5/multiplication table.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int N;
+    cin >> N;  
+    
+    for (int i = 1; i <= 12; i++) {
+        cout << N << " * " << i << " = " << N * i << endl;
+    }
+
+    return 0;
+}
+
+  
+    cout << "Even: " << even << endl;
+    cout << "Odd: " << odd << endl;
+    cout << "Positive: " << positive << endl;
+    cout << "Negative: " << negative << endl;
+    
+    return 0;
+}
+
